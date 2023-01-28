8935759437301674d2e1118fc1c338978246faf9
mkconan <koutarouelectro@gmail.com>
2023-01-28 10:25:27 +0900

    for RaspberryPi

--- update_sumishin_mf.py
+++ update_sumishin_mf.py
@@ -10,6 +10,7 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 
 # MoneyForwardの二段階認証を突破するためのもの
 load_dotenv()
@@ -23,8 +24,11 @@ def web_driver_setting() -> WebDriver:
     Returns:
         WebDriver:
     """
-    driver = webdriver.Chrome()
-    driver.implicitly_wait(2)
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")        
+    options.add_argument(f'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36')
+    driver = webdriver.Chrome(options=options)
+    driver.implicitly_wait(2)    
     return driver
 
 
